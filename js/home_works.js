c12d5b6dd015e9a938c80f0bcb6ef5551f845f09
Edil Zholbolduev <tashtan841@gmail.com>
2023-08-10 18:40:18 +0200

    first, i did validation of gmail, second I add animatiom move for small square

--- js/home_works.js
+++ js/home_works.js
@@ -1,5 +1,8 @@
+
+// homework (part 1)
 let input = document.querySelector('#gmail_input')
 let btn = document.querySelector('#gmail_button')
+let result = document.querySelector('#gmail_result')
 
 // let regExp = /[0-9a-zA-Z.@]/gi
 
@@ -17,9 +20,14 @@ btn.addEventListener('click', ()=>{
    let inputEmail = input.value.trim()
 
    if(checkEmail(inputEmail)){
-      alert('Successful')
+      result.innerHTML = 'Successfully added'
+      result.style.color = 'green'
+      input.value = ""
    }else{
-      alert('Not true')
+      result.innerHTML ='NOT TRUE!'
+      result.style.color ='red'
+      input.value = ""
+      
    }   
 
 })
@@ -28,5 +36,21 @@ function checkEmail(email){
    let regExp = /^[0-9a-zA-Z._%+-]+@gmail\.com$/
    return regExp.test(email)
 }
+// homework (part 2)
+let parentBlock = document.querySelector('.parent_block')
+let childBlock = document.querySelector('.child_block')
 
+let blockPlace = 0 
+let go = 2
 
+let moves = () =>{
+   blockPlace += go 
+   childBlock.style.left = `${blockPlace}px`
+
+   if(blockPlace < parentBlock.clientWidth - childBlock.clientWidth){
+      requestAnimationFrame(moves)
+   }
+   
+}
+moves()
+ 
