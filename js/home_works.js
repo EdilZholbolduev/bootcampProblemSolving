f947c8ec42998fcfdb5043cd51913a6bf7aa4f7c
Edil Zholbolduev <tashtan841@gmail.com>
2023-08-20 17:11:59 +0200

    add new JSON file, and some small changes

--- js/home_works.js
+++ js/home_works.js
@@ -107,3 +107,46 @@ resume.addEventListener('click',()=>{
    stop.disabled = false
    resume.disabled = true
 })
+// working with JSON file (practive json)
+const btnJson = document.querySelector('#btn-json')
+// part one
+btnJson.addEventListener('click',()=>{
+   const request = new XMLHttpRequest()
+   request.open("GET","../data.json")
+   request.setRequestHeader("Content-type","application/json")
+   request.send()
+   // part two(in part one we have an asynchronous, that's why we need give load make second part also asynchronous)
+   request.addEventListener('load',()=>{
+      const data = JSON.parse (request.response)
+      document.querySelector('.my-name').innerHTML = data.name
+      document.querySelector('.my-age').innerHTML = data.age
+   })
+})
+
+// people (json)
+const btnPeople = document.querySelector('.btn-json')
+const peopleContainer = document.querySelector('#people')
+
+function dataPeople(){ 
+const people = new XMLHttpRequest()
+people.open("GET","../people.json")
+people.setRequestHeader("Content-type","application/json")
+people.send()
+people.addEventListener('load',()=>{
+   const data2 =  JSON.parse (people.response)
+   data2.forEach((person)=>{
+      const div = document.createElement('div')
+      div.classList.add('card')
+      peopleContainer.appendChild(div)
+      div.innerHTML = `
+      <p>Worker:</p>
+      <h3>${person.name}</h3>
+      <h3>${person.age}</h3>
+      `
+btnPeople.removeEventListener('click', btnPeople)
+   })
+   btnPeople.removeEventListener('click',dataPeople
+   )
+})
+}
+btnPeople.addEventListener('click', dataPeople)
